f7e248a8a8e60a9435cbcb95609a98fb2b685853
Brett <norioz@gmail.com>
2021-10-26 14:34:50 -0700

    Remove static files from service.

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var cors = require("cors");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
-var indexRouter = require("./routes/index");
 var feedbackRouter = require("./routes/feedback");
 
 var app = express();
@@ -19,9 +17,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
 app.use("/feedback", feedbackRouter);
 
 // catch 404 and forward to error handler
